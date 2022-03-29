e6be43c0b9ea411574a60a78fe3bc685928f6c66
yusufadhiwinata <yusufadhiwinata80@gmail.com>
2022-03-29 14:19:29 +0800

    third

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import Splash from './splash/Splash';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <View style={{flex: 1, justifyContent: 'center'}}>
       <Splash />
